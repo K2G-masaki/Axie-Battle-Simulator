a3a77282f7debbd59346fe85493b9a4306fc5435
K2G-masaki <guitarmasaki25@gmail.com>
2021-11-01 20:47:05 +0900

    Initial Commit

---
+++ Axie Battle Simulator/Axie_Battle_SimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Axie_Battle_SimulatorApp.swift
+//  Axie Battle Simulator
+//
+//  Created by masaki on 2021/11/01.
+//
+
+import SwiftUI
+
+@main
+struct Axie_Battle_SimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Axie Battle Simulator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Axie Battle Simulator
+//
+//  Created by masaki on 2021/11/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
